3b806cbfb7b7a557dc071becd85a736fb750f988
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-07-06 10:04:11 -0700

    Tag route GET by id working

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       // JOIN with travellers, using the Trip through table
-      include: [{ model: Product}]
+      include: [{ model: Product, through: ProductTag}]
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
@@ -37,6 +37,12 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
